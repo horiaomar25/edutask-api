269bd0296f38758e1cd0f5190f1dd5e866d94db6
horiaomar25 <horiaomar6@gmail.com>
2025-03-19 14:06:08 +0000

    added: update for deployment

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,11 @@
-
+// filepath: c:\Users\Horia Omar\OneDrive\Pictures\Documents\Coding Projects\Main Projects\edutask-api\server\db\index.js
 import 'dotenv/config';
-import pg from "pg";
+import pg from 'pg';
 
 const connectionString = process.env.DB_CONNECTION_STRING;
 
 if (!connectionString) {
-    throw new Error("No connection string defined");
+    throw new Error('No connection string defined');
 }
 
 export const pool = new pg.Pool({
--- server/db/scripts/reset-database.js
+++ server/db/scripts/reset-database.js
@@ -2,29 +2,27 @@ import { pool } from "../index.js";
 
 async function resetDatabase() {
     try {
-        //Drop existing tables if they exists
-    await pool.query(`
-    DROP TABLE IF EXISTS tasks CASCADE;
-    `);
+        // Drop existing tables if they exist
+        await pool.query(`
+        DROP TABLE IF EXISTS tasks CASCADE;
+        `);
 
-    // GENERATED means that the ID is generated and you don't need to set it when inserting a record into the DB
-    //create Tasks table 
-    await pool.query(`
-    CREATE TABLE tasks (
-        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-        task_name TEXT,
-        task_description TEXT,
-        task_date DATE,
-        task_type TEXT,
-        completed BOOLEAN)`);
+        // Create Tasks table
+        await pool.query(`
+        CREATE TABLE tasks (
+            id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+            task_name TEXT,
+            task_description TEXT,
+            task_date DATE,
+            task_type TEXT,
+            completed BOOLEAN
+        )`);
 
-    console.log("Database reset successful")
-
-
+        console.log("Database reset successful");
     } catch (error) {
         console.error("Database reset failed: ", error);
     } finally {
-        //end the pool
+        // End the pool
         await pool.end();
     }
 }
